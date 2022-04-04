25bec6cf1b578a5668946edfaecafe2d2a17dc3a
Kgiunta <giunta.kyle@yahoo.com>
2022-04-04 17:46:48 -0400

    puesdo coding questions

--- index.js
+++ index.js
@@ -2,12 +2,80 @@
 
 // TODO: Create an array of questions for user input
 const questions = [];
+// What is the name of your project?
+// const projectTitle =
+
+// Please Provide a description of your project
+// const description =
+
+//  "Chose the appropriate license for this project: ",
+
+//                 "Academic",
+//                 "GNU",
+//                 "ISC",
+//                 "MIT",
+//                 "Mozilla",
+//                 "Open"
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// fs.appendFile('README.md', `${process.argv[2]}\n`, (err)
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+// # ${projectTitle}
+/* <Your-Project-Title> */
+
+// ## Description
+
+// ${description}
+//
+
+// ## Table of Contents
+
+// Below are the table of contents for this application:
+
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [Credits](#credits)
+// - [License](#license)
+
+// ## Installation
+
+// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+// ## Usage
+
+// Provide instructions and examples for use. Include screenshots as needed.
+
+// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+//     ```md
+//     ![alt text](assets/images/screenshot.png)
+//     ```
+
+// ## Credits
+
+// List your collaborators, if any, with links to their GitHub profiles.
+
+// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+// If you followed tutorials, include links to those here as well.
+
+// ## License
+
+// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+// ---
+
+// 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+// ## Badges
+
+// ## Features
+
+// If your project has a lot of features, list them here.
